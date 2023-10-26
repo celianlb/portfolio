f0c096e9e1ae54889f35768df3fab6c82b2c3b9b
Célian LEBACLE <celianlebacle06@gmail.com>
2023-10-26 21:12:01 +0200

    first v

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Education } from "../../../typing";
+import { Education } from "../../typing";
 
 export default function AllEducation({ education }: { education: Education[] }) {
   education = education.sort((a, b) => a.year - b.year );
   return (
     <div className="flex flex-col gap-4 mx-auto md:flex-row md:justify-between md:mx-0">
       {education.map((education) => (
-        <div className="flex flex-row gap-4 text-secondary cursor-default items-center">
+        <div key={education._id} className="flex flex-row gap-4 text-secondary cursor-default items-center">
           <p className="text-xl font-bold font-montserrat rounded-2xl bg-secondary bg-opacity-10 p-6">
             {education.year}
           </p>

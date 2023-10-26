2b65bbf912c6cfc4ec274a8913af4c2374484f86
Célian LEBACLE <celianlebacle06@gmail.com>
2023-10-26 21:23:30 +0200

    first v

--- src/app/projects/[slug]/loading.tsx
+++ src/app/projects/[slug]/loading.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/Header";
 import Spinner from "@/components/Spinner";
 
 
 export default function OneProjectLoading() {
   return (
       <div>
+        <Header />
         <Spinner />
       </div>
   );
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Section from "@/components/Section";
 import ProjectCard from "./ProjectCard";
+import Header from "@/components/Header";
 
 
 export default async function ProjectsPage() {
@@ -7,6 +8,7 @@ export default async function ProjectsPage() {
   return (
     <main>
       <Section>
+        <Header />
         <ProjectCard />
       </Section>
     </main>

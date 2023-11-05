0c82c4e853c49430c1e85354de237a4fda86f7ec
Célian LEBACLE <celianlebacle06@gmail.com>
2023-11-06 00:00:13 +0100

    footer and loader

--- src/app/projects/[slug]/loading.tsx
+++ src/app/projects/[slug]/loading.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
 import Spinner from "@/components/Spinner";
 
 
 export default function OneProjectLoading() {
   return (
       <div>
-        <Header />
         <Spinner />
       </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <div className="pt-8">
       <footer className="flex flex-col gap-4 items-center md:flex-row md:justify-between">
           <SocialComponents width={48} height={48} />
-          <p className=" text-secondary font-rnssanz">
-            © 2023 Célian Lebacle. Tous droits réservés.
+          <p className=" opacity-50 text-secondary font-rnssanz">
+            © 2023 Célian Lebacle. All rights reserved.
           </p>
       </footer>
     </div>
